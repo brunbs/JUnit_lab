059a7ac3262f53c22614fb6f115f14f415320157
brunbs <babosa227@gmail.com>
2022-07-07 17:32:13 -0300

    Create test for checking if person is of age

---
+++ src/main/java/bruno/junit/Person.java
@@ -0,0 +1,43 @@
+package bruno.junit;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+
+public class Person {
+
+    private String name;
+    private LocalDate birthdate;
+
+    public Person(String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birthdate;
+    }
+
+    public void setBirth(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getAge() {
+        return (int) ChronoUnit.YEARS.between(this.birthdate, LocalDate.now());
+    }
+
+    public boolean isOfAge() {
+        if(this.getAge() >= 18) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/bruno/junit/PersonTest.java
@@ -0,0 +1,28 @@
+package bruno.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.List;
+
+public class PersonTest {
+
+    @Test
+    void shouldReturnIfOfAge() {
+        Person bruno = new Person("Bruno", LocalDate.of(2020, 7, 1));
+        Assertions.assertFalse(bruno.isOfAge());
+
+        Person cassia = new Person("Cassia", LocalDate.of(1991, 7, 5));
+        Assertions.assertTrue(cassia.isOfAge());
+
+        List<Person> people = List.of(new Person("João", LocalDate.now()), new Person("Júlia", LocalDate.now()));
+
+        Assertions.assertAll("people",
+                () -> Assertions.assertFalse(people.get(0).isOfAge()),
+                () -> Assertions.assertFalse(people.get(1).isOfAge())
+        );
+    }
+
+}
